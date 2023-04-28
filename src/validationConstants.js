2f09b95ecad27d811b7b93c4e6dbd223d20cec8a
Dušan Nikodijević <dusan.nikodijevic@eng.it>
2023-04-28 13:26:41 +0200

    init

---
+++ src/validationConstants.js
@@ -0,0 +1,21 @@
+export const firstNameRules = [
+    (value) => !!value || "First name is required",
+    (value) => value.length >= 3 || "Name must be at least 3 characters",
+];
+export const lastNameRules = [
+    (value) => !!value || "Last name is required",
+    (value) => value.length >= 3 || "Name must be at least 3 characters",
+];
+export const indexYearRules = [
+    (value) => !!value || "Index year is required",
+    (value) => (value >= 2000 && value <= 2100) || "Index year must be between 2000 and 2100",
+];
+export const indexNumberRules = [
+    (value) => !!value || "Index number is required",
+    (value) => value.length === 4 || "Name must be 4 characters",
+];
+export const currentYearOfStudyRules = [
+    (value) => !!value || "Current year of study is required",
+    (value) => value.length == 1 || "Current year of study must be  1 character",
+];
+
